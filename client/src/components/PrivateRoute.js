c6ebd2791e98ad68932f81b89da6daeb707a9c85
kirkby <kirkby@neighbor.com>
2020-12-03 20:16:51 -0700

    lecture work-in-progress

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-const PrivateRoute = (props) => {
+/*
+  1. it has the same interface a Route
+  2. it renders a <Route /> and pass props to it
+  3. it checks if the user has an authentication 
+     token and if they do, it renderd the passed in component
+  4. if the user dos not have an authentication token, it redirects to /login
+*/
+const PrivateRoute = ({ component: Component, ...props }) => {
   return <Route {...props} />
 }
 
