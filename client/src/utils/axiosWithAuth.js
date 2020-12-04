6c8ddc7003bea4f2c50d9d929db7fdb734f2c53b
kirkby <kirkby@neighbor.com>
2020-12-03 21:07:16 -0700

    lecture work-in-progress

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -4,6 +4,7 @@ const axiosWithAuth = () => {
   const token = localStorage.getItem('token');
 
   return axios.create({
+    baseUrl: "http://localhost:5000/api",
     headers: {
       Authorization: token
     }
