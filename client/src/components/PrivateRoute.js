90470af835f56a61b4c9a2f6970adc8644d3ec81
kirkby <kirkby@neighbor.com>
2021-02-11 21:21:40 -0700

    updating project

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route } 'react-router-dom';
+
+/* 1. same interface (API) as Route
+   2. render a <Route /> passing all the properties it was passed to it
+   3. check to see if the user has an auth token, if not, redirect back to login
+   4. if have a token, 2. will render GasPrices retrieve data
+*/
+
+const PriveRoute = props => {
+
+}
+
+export default PrivateRoute;
